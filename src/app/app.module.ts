2ea629be93941ac0d89ce31c8f5e7a8f5187652c
novelkhan <novel4004@gmail.com>
2024-07-26 00:31:38 +0600

    Footer component is created inside app module in it's dedicated components folder using 'ng g c footer --skip-tests' command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
