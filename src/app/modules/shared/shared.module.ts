febfa63644e5466186fc17107841a44ab25aaf3a
novelkhan <novel4004@gmail.com>
2024-07-26 23:32:51 +0600

    not-found component is created inside shared module in it's dedicated components/errors folder using 'ng g c not-found --skip-tests' command

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotFoundComponent
+  ],
   imports: [
     CommonModule
   ]
