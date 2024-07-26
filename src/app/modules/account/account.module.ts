08beb3168298e473ab8fe5a0ca347a762c610e84
novelkhan <novel4004@gmail.com>
2024-07-26 21:31:03 +0600

    Register component is created inside account module in it's dedicated components folder using 'ng g c register --skip-tests' command

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { ConfirmEmailComponent } from './components/confirm-email/confirm-email.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     ConfirmEmailComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
