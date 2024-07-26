0fb1f82e9964458ce3e45ec6e3d6ec88e38be9c4
novelkhan <novel4004@gmail.com>
2024-07-26 21:34:10 +0600

    reset-password component is created inside account module in it's dedicated components folder using 'ng g c reset-password --skip-tests' command

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -5,13 +5,15 @@ import { AccountRoutingModule } from './account-routing.module';
 import { ConfirmEmailComponent } from './components/confirm-email/confirm-email.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 
 @NgModule({
   declarations: [
     ConfirmEmailComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ResetPasswordComponent
   ],
   imports: [
     CommonModule,
