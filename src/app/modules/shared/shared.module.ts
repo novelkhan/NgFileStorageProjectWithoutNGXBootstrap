2151129a0b742d62ed2fcbfbddec2721447c2a05
novelkhan <novel4004@gmail.com>
2024-07-26 23:35:21 +0600

    validation-messages component is created inside shared module in it's dedicated components/errors folder using 'ng g c validation-messages --skip-tests' command

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/errors/not-found/not-found.component';
+import { ValidationMessagesComponent } from './components/errors/validation-messages/validation-messages.component';
 
 
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    ValidationMessagesComponent
   ],
   imports: [
     CommonModule
