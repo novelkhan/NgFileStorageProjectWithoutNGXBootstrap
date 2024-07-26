ea601100b15f0b77dd0eace960285098ba7484c9
novelkhan <novel4004@gmail.com>
2024-07-26 23:37:43 +0600

    Notification component is created inside shared module in it's dedicated components/modals folder using 'ng g c notification --skip-tests' command

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 import { ValidationMessagesComponent } from './components/errors/validation-messages/validation-messages.component';
+import { NotificationComponent } from './components/modals/notification/notification.component';
 
 
 
 @NgModule({
   declarations: [
     NotFoundComponent,
-    ValidationMessagesComponent
+    ValidationMessagesComponent,
+    NotificationComponent
   ],
   imports: [
     CommonModule
